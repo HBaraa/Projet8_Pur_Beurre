51da40f9e984411309d0e77b81ef7f11c23df447
Baraa <hamdibaraa42@gmail.com>
2022-11-06 14:23:01 +0100

    correcting the model

--- nutella/product/models.py
+++ nutella/product/models.py
@@ -6,6 +6,3 @@ class Products(models.Model):
     name = models.fields.CharField(max_length=100)
     category = models.fields.CharField(max_length=100)
     nutriscore = models.fields.CharField(max_length=1)
-    brands = models.CharField(verbose_name="Marques du produit", max_length=100)  # noqa
-    link = models.URLField(unique=True, null=True, blank=True)
-    image = models.URLField(null=True, blank=True)
