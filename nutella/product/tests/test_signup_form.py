261d2684df388a6e5bee92a6b0dcf0d39472e9f6
Baraa <hamdibaraa42@gmail.com>
2022-11-27 20:24:07 +0100

    correcting test_signup_form.py fileld

--- nutella/product/tests/test_signup_form.py
+++ nutella/product/tests/test_signup_form.py
@@ -6,20 +6,21 @@ class TestSignUpForm(TestCase):
     def test_custom_user_creation_form_email_field_label(self):
         form = SignUpForm()
         self.assertTrue(
-            form.fields["email"].label == None or form.fields["email"].label == "Email"
+            form.fields["email"].label == None  # noqa
+            or form.fields["email"].label == "Email"
         )
 
     def test_custom_user_creation_form_first_name_field_label(self):
         form = SignUpForm()
         self.assertTrue(
-            form.fields["first_name"].label == None
+            form.fields["first_name"].label == None  # noqa
             or form.fields["first_name"].label == "First name"
         )
 
     def test_custom_user_creation_form_second_name_field_label(self):
         form = SignUpForm()
         self.assertTrue(
-            form.fields["second_name"].label == None
+            form.fields["second_name"].label == None  # noqa
             or form.fields["second_name"].label == "Second name"
         )
 
