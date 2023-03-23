4d37ef083d248a1828bd305e9e3f44a2bbe2c345
Baraa <hamdibaraa42@gmail.com>
2023-03-23 19:56:34 +0100

    last changes

--- nutella/settings.py
+++ nutella/settings.py
@@ -59,7 +59,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-OPENFOODFACTS_PAGE_SIZE = os.environ.get("OPENFOODFACTS_PAGE_SIZE", 1000)
+OPENFOODFACTS_PAGE_SIZE = os.environ.get("OPENFOODFACTS_PAGE_SIZE", 100)
 
 # Application definition
 
