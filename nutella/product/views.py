6a3b696727b379955d6b2dc4e02e9de72db99dcc
Baraa <hamdibaraa42@gmail.com>
2023-03-23 20:37:06 +0100

    changes

--- nutella/product/views.py
+++ nutella/product/views.py
@@ -34,7 +34,7 @@ def mention(request):
 
 class MyLoginView(auth_views.LoginView):
     template_name = "accounts/login.html"
-    success_url = "home"
+    success_url = ""
 
 
 def signup(request):
