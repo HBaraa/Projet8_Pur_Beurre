5478a52c7dbc43040ed725c882660a3d725ee583
Baraa <hamdibaraa42@gmail.com>
2023-05-02 20:34:10 +0200

    changing settings

--- nutella/settings.py
+++ nutella/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # configurer les serveurs autorisés à accéder à votre app
