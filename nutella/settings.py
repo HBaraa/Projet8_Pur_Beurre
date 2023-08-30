8844984c089fe168e7df2014c6672739ccd177b2
Baraa <hamdibaraa42@gmail.com>
2023-08-30 18:19:45 +0200

    change settings

--- nutella/settings.py
+++ nutella/settings.py
@@ -43,10 +43,12 @@ else:
 
 
 # configurer les serveurs autorisés à accéder à votre app
-
 DATABASES = {}
+DATABASE_URL = 'postgresql://<postgresql>'
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+# DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 # DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
